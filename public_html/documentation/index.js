63eade26a999992c926e6dae9a5a4cfe8463bee9
RSBalchII <128771311+RSBalchII@users.noreply.github.com>
2023-05-08 14:14:18 -0600

    made function to change color of selected elements

--- public_html/documentation/index.js
+++ public_html/documentation/index.js
@@ -1,1 +1,7 @@
+function setColor(id, color) {
+    const element = document.getElementById(id);
+    element.style.backgroundColor = color;
+}
 
+setColor('upper-section', '#B6D7B9');
+setColor('canvas', '#9ABD97')
