31abdc5386076be211395865b5161d3be2f5589f
RSBalchII <128771311+RSBalchII@users.noreply.github.com>
2023-05-17 23:10:17 -0600

    trying out lightbox2

---
+++ public_html/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // Initialize Lightbox2
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    lightbox.option({
+    });
+});
